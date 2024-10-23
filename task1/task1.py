4e1cb4ca5ebb9bf02b1e7caafb52a17bd0cef331
XYU <thfjfjfjdjfh@gmail.com>
2024-10-23 20:38:29 +0700

    file input

--- task1/task1.py
+++ task1/task1.py
@@ -8,28 +8,33 @@ class Node:
 
 ''' INPUT '''
 
-number_of_tables = int(input())
-number_of_rows_in_tables = [int(x) for x in input().split()]
-number_of_attributes = int(input())
-attributes = []
-for i in range(number_of_attributes):
-    table_num, filter_attr, card_value = input().split()
-    attributes.append([int(table_num), filter_attr, int(card_value)])
+f = open("input.txt")
+number_of_tables = int(f.readline().strip())
+number_of_rows_in_tables = [0]
+for x in f.readline().strip().split():
+    number_of_rows_in_tables.append(int(x))
 
-number_of_scan_predicats = int(input())
-scan_preds = []
+number_of_attributes = int(f.readline().strip())
+attributes = [[] for _ in range(number_of_tables + 1)]
+for i in range(number_of_attributes):
+    table_num, filter_attr, card_value = f.readline().strip().split()
+    attributes[int(table_num)].append([int(table_num), filter_attr, int(card_value)])
+
+number_of_scan_predicats = int(f.readline().strip())
+scan_preds = [[] for _ in range(number_of_scan_predicats + 1)]
 for i in range(number_of_scan_predicats):
-    table_num, filter_attr = input().split()
+    table_num, filter_attr = f.readline().strip().split()
     scan_preds.append([int(table_num), filter_attr])
     
-number_of_join_predicats = int(input())
+number_of_join_predicats = int(f.readline().strip())
 join_preds = []
 for i in range(number_of_join_predicats):
-    table_num1, table_num2, join_table1_attr, join_table1_attr = input().split()
-    join_preds.append([int(table_num1), int(table_num2), join_table1_attr,join_table1_attr])
+    table_num1, table_num2, join_table1_attr, join_table2_attr = f.readline().strip().split()
+    join_preds.append([int(table_num1), int(table_num2), join_table1_attr, join_table2_attr])
 
 ''' COSTS '''
 scan_costs = []
 
-print(number_of_rows_in_tables)
-print(attributes)
+print(number_of_rows_in_tables[2])
+print(attributes[2])
+print(join_preds)
